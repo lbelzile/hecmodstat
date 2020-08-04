3345a94410edbbaeb282aa27ab74037362d8d765
Léo Belzile <belzilel@gmail.com>
2020-08-04 13:34:00 -0400

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,123 @@
+#' @import ggplot2 poorman viridis learnr
+NULL
+
+#' Prix de billets de train
+#'
+#' Un sous-ensemble de billets de trains pour le trajet entre Madrid et Barcelone
+#' vendus par la Renfe pour trois types de trainoit Alta Velocidad Española (\code{AVE}),
+#' soit Alta Velocidad Española conjointement avec TGV (un partenariat entre la SNCF et Renfe
+#' pour les trains à destination ou en provenance de Toulouse) \code{AVE-TGV}, soit les trains régionaux \code{REXPRESS};
+#'  seuls les trains étiquetés \code{AVE} ou \code{AVE-TGV} sont des trains à grande vitesse.
+#'
+#' @format Une base de données de 10000 observations contenant les variables suivantes:
+#' \describe{
+#'   \item{prix}{prix du billet (en euros)}
+#'   \item{dest}{facteur, indicateur binaire du trajet, soit de Barcelone vers Madrid ou de Madrid vers Barcelone}
+#'   \item{tarif}{facteur, variable catégorielle indiquant le tarif du billet, un parmi \code{AdultoIda}, \code{Promo} et \code{Flexible}}
+#'   \item{classe}{facteur classe du billet, soit  \code{Preferente}, \code{Turista}, \code{TuristaPlus} ou \code{TuristaSolo}}
+#'   \item{type}{facteur variable catégorielle indiquant le type de train, \code{AVE-TGV}, \code{AVE} ou \code{REXPRESS}.}
+#'   \item{duree}{longueur annoncée du trajet (en minutes)}
+#'   \item{jour}{facteur entier indiquant le jour de la semaine du départ allant de dimanche (\code{1}) à samedi (\code{7})}
+#' }
+#' @source Kaggle \url{https://www.kaggle.com/thegurusteam/spanish-high-speed-rail-system-ticket-pricing}
+"renfe"
+
+#' Primes d'assurance santé
+#'
+#' Le fichier de données simulées \code{assurance} contient de l'information sur les frais médicaux
+#' facturés à 1338 adultes américains assurés au courant de l'année 2003.
+#'
+#' @format Une base de données de 1338 observations avec
+#' \describe{
+#'   \item{age}{âge (en années)}
+#'   \item{sexe}{sexe, \code{homme} ou \code{femme}}
+#'   \item{imc}{indice de masse corporelle (en kg/mètres carrés)}
+#'   \item{enfant}{nombre d'enfants à charge}
+#'   \item{fumeur}{\code{oui} pour les fumeurs, \code{non} autrement.}
+#'   \item{region}{lieu de résidence, une région parmi \code{sudouest}, \code{sudest}, \code{nordouest} ou \code{nordest}}
+#'   \item{frais}{frais médicaux annuels en 2003 (en dollars USD).}
+#' }
+#'
+#' @source Lantz, Brett (2003), \emph{Machine Learning with R}, Packt Publishing.
+"assurance"
+
+#' Accidents de la route aux États-Unis
+#'
+#' @source \emph{National Highway Traffic Safety Administration} (NHTSA)
+#' @format Une base de données de 40 observations en format long contenant les 5 variables suivantes:
+#' \describe{
+#' \item{annee}{facteur décrivant l'année de référence, 2010 ou 2018}
+#' \item{region}{facteur indicant la région géographique telles que définies par la NHTSA}
+#' \item{popn}{entier, population totale estimée de la région}
+#' \item{nmorts}{entier, nombre de morts}
+#' \item{moment}{facteur indiquant quelle période le décompte couvre, soit jour ou nuit}
+#' }
+#'
+"accident"
+
+
+#' Affiliation politique d'Américain(e)s
+#'
+#' Données tirées du \emph{General Social Survey} (2000) qui recense
+#' le sexe et l'identification politique des sondés selon le parti vers lequel
+#' ils s'identifiaient le plus
+#'
+#' @format Un tableau de contingence deux par trois:
+#' \describe{
+#' \item{nombre}{décompte}
+#' \item{sexe}{sexe, soit \code{homme} ou \code{femme}}
+#' \item{parti}{facteur indiquant le parti, un parmi Démocrate (\code{democrate}), indépendant (\code{indep}) et GOP (\code{republicain})}
+#' }
+#'
+#' @source Tableau 2.5 de Agresti (2007), An Introduction to Categorical Data Analysis, Wiley.
+"affpol"
+
+#' Examens pratique de conduite
+#'
+#' Ces données, tirées de GovUK et nettoyées, donnent le taux de succès
+#' aux examens pratique de conduite en Grande-Bretagne pour 2018 dans 346 centres de conduite.
+#' Un total de 761 750 personnes on réussi l’examen pratique, pour 1 663 897 essais.
+#'
+#' @format Une base de données contenant les résultats suivants, regroupées par centre:
+#' \describe{
+#'  \item{reussite}{nombre de personnes ayant réussi l'examen (par sexe)}
+#'  \item{total}{nombre total de personne dans le centre (par sexe)}
+#'  \item{region}{facteur indiquant une des neuf régions d'Angleterre, le Pays de Galles ou l'Écosse (les données sont agrégées pour ces deux dernières)}
+#'  \item{sexe}{facteur, \code{homme} ou \code{femme}}
+#'  }
+#' @source \url{https://www.gov.uk/government/statistical-data-sets/driving-test-statistics-drt}
+"gbconduite"
+
+#' Distraction face à un obstacle
+#'
+#' Données appariées fictives relative à une étude du Tech3Lab
+#' comparant le temps de réaction (en secondes) de participants pour détecter
+#' l'apparition d'un bicyclette dans leur champ visuel
+#' exposés à deux mises en situations présentées dans un ordre aléatoire: texter
+#' ou parler en marchant au cellulaire.
+#'
+#' @format Une base de données de 35 observations
+#' \describe{
+#' \item{c}{temps de réaction en secondes lors d'une conversation}
+#' \item{c}{temps de réaction en secondes lors d'une conversation}
+#' }
+#' @source Marc Fredette
+"distraction"
+
+
+#' Résultats d'une étude de simulation sur les données Renfe
+#'
+#' Statistiques de test, valeur-p et différence de moyenne pour 1000
+#' échantillons aléatoires de taille 100 tirées de la base de données
+#' Renfe.
+#'
+#' @format Une base de données de 1000 observations
+#' \describe{
+#' \item{difmoy}{différence de moyenne de prix entre Madrid-Barcelone versus Barcelone-Madrid}
+#' \item{statW}{statistique de Welch pour le test de localisation pour deux échantillons}
+#' \item{icbi}{borne inférieure de l'intervalle de confiance basé sur la statistique de Welch}
+#' \item{icbs}{borne supérieure de l'intervalle de confiance basé sur la statistique de Welch}
+#' \item{ivalp}{valeur-p asymptotique}
+#' }
+#' @source Léo Belzile
+"renfe_simu"
---
+++ data-raw/DATASET.R
@@ -0,0 +1,116 @@
+## code to prepare `DATASET` dataset goes here
+
+library(dplyr)
+library(haven)
+library(fs)
+library(usethis)
+
+setwd("/home/lbelzile/Documents/Dropbox/Teaching/2020W/MATH60619A/Données/Français")
+lfile <- list.files(pattern = ".sas7bdat")
+external_env <- new.env()
+for(file in lfile){
+  filen <- sub('\\.sas7bdat$', '', file)
+  data <- haven::read_sas(file)
+  data <- data %>% mutate(across(where(is.character), factor))
+  if(filen == "accident"){
+    data  <-  data  %>%
+      mutate(annee = factor(annee),
+        region = factor(region))
+  } else if(filen == "renfe"){
+    data <- data %>%
+      mutate(dest = factor(recode(dest, "0" = "Barcelone-Madrid",
+                                  "1" = "Madrid-Barcelone")),
+             jour = factor(jour))
+  } else if(filen == "automobile"){
+    data$nom <- NULL
+    data <- data %>%
+      mutate(cylindre = factor(cylindre),
+             origine = factor(origine))
+  } else if(filen == "billets"){
+    data <- data %>%
+      mutate(groupe = factor(groupe))
+  } else if(filen == "bixiuni"){
+    data <- data %>%
+    mutate(date = lubridate::ymd(as.character(date)))
+  } else if(filen == "casque"){
+    data <- data %>%
+      rename(modele=Modele,
+             impact = Impact,
+             gadd = Gadd) %>%
+      mutate(modele = factor(modele),
+             impact = factor(impact))
+  } else if(filen == "consommation"){
+  data <- data %>%
+      mutate(habitude = factor(habitude),
+             marque = factor(marque))
+  } else if(filen == "delai"){
+    data <- data %>%
+      mutate(delai = factor(delai),
+             stade = factor(stade))
+  } else if(filen == "enfantsfiji"){
+    data <- data %>%
+      mutate(dur = factor(dur),
+             res = factor(res),
+             educ = factor(educ))
+  } else if(filen == "goldstein"){
+      data <- data %>%
+        mutate(ecole = factor(ecole),
+               rv = factor(rv))
+  } else if(filen == "gsce"){
+    data <- data %>%
+      mutate(centre = factor(centre))
+  } else if(filen == "intention"){
+    data <- data %>%
+      mutate(revenu = factor(revenu),
+             educ = factor(educ))
+  } else if(filen == "mobilisation"){
+    data <- data %>%
+      mutate(idunite = factor(idunite))
+  } else if(filen == "prix"){
+    data <- data %>%
+      mutate(prog = factor(prog))
+  } else if(filen == "renergie"){
+    data <- data %>%
+      mutate(region = factor(region),
+             date = lubridate::ymd(date))
+  } else if(filen == "salaireprof"){
+    data <- data %>%
+      mutate(echelon = factor(echelon))
+  } else if(filen == "servqual"){
+    data <- data %>%
+      mutate(banque = factor(banque))
+  } else if(filen == "tolerance"){
+    data <- data %>%
+      mutate(id = factor(id))
+ } else if(filen == "vengeance"){
+  data <- data %>%
+    rename(temps=t) %>%
+    mutate(id = factor(id),
+           temps = factor(temps))
+  } else if(filen == "heures"){
+      data <- data %>%
+        rename(heures = HEURES)
+  }
+  assign(x = filen, value = data, envir = external_env)
+}
+rm(data, filen)
+external_env$bixicoll <- external_env$bixi
+external_env$bixi <- external_env$bixiuni
+rm(covid, envir = external_env)
+
+
+ls(external_env)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+path <- usethis::proj_get()
+dir_data <- path(path, "data")
+dir_create(dir_data)
+objs <- ls(envir = external_env)
+paths <- path(dir_data, objs, ext = "rda")
+mapply(
+  save,
+  list = objs,
+  file = paths,
+  MoreArgs = list(envir = external_env, compress = "bzip2", version = 3)
+)
+
+
